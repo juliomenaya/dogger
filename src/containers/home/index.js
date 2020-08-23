039d2c25994ec295d0639fafa0e275b2f057df21
MikeGsus <mike08ahj@hotmail.com>
2020-08-23 13:59:05 -0500

    Create Home container

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import { Container } from './styled'
+import {
+  Container,
+  Logo,
+  TextContainer,
+  Title,
+  TitleContainer,
+  SubTitle,
+  Paragraph
+} from './styled'
 
 const Home = () => {
   return (
-    <Container />
+    <Container>
+      <Logo src={require('../../assets/img/png/logo/dogger_logo.png')} alt='Dogger' />
+      <TextContainer>
+        <TitleContainer>
+          <Title align='right'>¿Necesitas pasear a tu perro?</Title>
+          <Title align='left'>¿No tienes tiempo para ello?</Title>
+          <Title align='right'>¿Necesitas alguien de confianza?</Title>
+        </TitleContainer>
+        <SubTitle>Dogger es para tí</SubTitle>
+        <Paragraph>Es una plataforma donde dueños de perros pueden contactar con personas dispuestas a pasear a sus lomitos.</Paragraph>
+      </TextContainer>
+    </Container>
   )
 }
 
--- src/containers/home/styled.js
+++ src/containers/home/styled.js
@@ -1,7 +1,47 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  diplay: flex;
-  justify-content: center;
   align-items: center;
+  background: #FFFFF9;
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap-reverse;
+  justify-content: space-evenly;
+  padding: 0 5%;
+`
+
+export const Logo = styled.img`
+  flex: 1;
+`
+
+export const TextContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  height: 90%;
+  justify-content: space-evenly;
+  text-align: justify;
+`
+
+export const Title = styled.h3`
+  font-size: 2rem;
+  margin: 7.5px;
+  ${({align}) => `text-align: ${align};`}
+`
+
+export const TitleContainer = styled.h3`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  text-align: justify;
+`
+
+export const SubTitle = styled.h3`
+  font-size: 2.5rem;
+`
+
+export const Paragraph = styled.p`
+  font-size: 1.5rem;
 `
