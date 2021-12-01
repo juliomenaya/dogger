da12d36618bddc715d2759802387b973e73d64f0
juliomenaya <juliomenaya@gmail.com>
2021-12-01 02:27:45 -0600

    admin separated by app

--- server/dogs/admin.py
+++ server/dogs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dogs, DogSize, Schedules
 
-# Register your models here.
+admin.site.register(Dogs)
+admin.site.register(DogSize)
+admin.site.register(Schedules)
